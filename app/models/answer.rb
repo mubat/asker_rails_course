27646cce06f5fd83cd5c8efe1055a89a40bb01ca
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-01 18:49:59 +0300

    add association with Question to Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ApplicationRecord
+  belongs_to :question, required: true
 
   validates :body, presence: true
 end
--- db/migrate/20210801152822_create_answers.rb
+++ db/migrate/20210801152822_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.text :body, null: false
+      t.references :question, foreign_key: true, null: false
 
       t.timestamps
     end
