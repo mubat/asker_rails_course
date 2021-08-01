9873eee14f55470ed23434e0dfa4716bc55266cc
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-01 18:49:29 +0300

    correct association description in test

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should have_one(:question).required }
+  it { should belong_to(:question).required }
 
   it { should validate_presence_of :body }
 end
