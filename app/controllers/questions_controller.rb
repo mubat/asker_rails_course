3bee84f9a1b81b8959ffbe4da76b8ffdab2f787d
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-07 20:05:32 +0300

    create action handler for Question:new action and tests for it

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,11 +10,24 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
   def show; end
 
   def edit; end
 
   def load_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
