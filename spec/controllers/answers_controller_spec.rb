e71a596fbafd55239f0933c148f08af64fb8182f
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-07 22:52:32 +0300

    add test descriptions for Answer's `new` and `create` actions

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe 'GET #new - User can open creation form of Answer' do
 
+    it 'opens under question resource'
+    it 'render `new` form'
+
+  end
+
+  describe 'GET #create - User can create new Answer' do
+    it 'opens under question resource'
+
+    context 'with valid parameters' do
+      it 'saves a new Answer'
+      it 'redirects to Question\'s `show` view'
+    end
+
+    context 'with invalid parameters' do
+      it 'don\'t saves a new invalid Answer'
+      it 'reopen the `new` view'
+    end
+  end
 end
