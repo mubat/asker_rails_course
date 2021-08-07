83a1b84b3bcfeee3cab0a0132a814a3d3c9f4f0c
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-07 19:49:21 +0300

    create action handler for Question:edit action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,4 +11,8 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
 end
