6a21724a90fc5a508749374dbc2c4b92f5260642
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-09 23:00:10 +0300

    refactor sign_in test - move common code to global test scope

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -6,12 +6,13 @@ feature 'User sign in', '
   I want to be able to sign in
 ' do
 
-  scenario 'Registered user try to sign in' do
-    User.create!(email: 'user@test.com', password: '123123123')
+  given(:user) { User.create!(email: 'user@test.com', password: '123123123') }
 
-    visit new_user_session_path
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '123123123'
+  background { visit new_user_session_path }
+
+  scenario 'Registered user try to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully'
@@ -19,7 +20,6 @@ feature 'User sign in', '
   end
 
   scenario 'Non-registered user try to sign in' do
-    visit new_user_session_path
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '123123123'
     click_on 'Log in'
