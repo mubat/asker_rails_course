9c4006008e051f997097128f46341fbc596438f2
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-09 23:17:33 +0300

    add functionality to ask a question (by test case)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully created.'
     else
       render :new
     end
