c408d6af48154e9650871caaf34600c3e59151c6
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-09 23:10:58 +0300

    describe test to ask a question

--- spec/acceptance/question/create_spec.rb
+++ spec/acceptance/question/create_spec.rb
@@ -5,7 +5,27 @@ feature 'User can create question', '
   As an authenticated user
   I\'d like to be able to ask question
 ' do
-  scenario 'Authenticated user asks a question'
+
+  given(:user) { User.create!(email: 'user@test.com', password: '123123123') }
+
+  scenario 'Authenticated user asks a question' do
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Test test test'
+    click_on 'Ask'
+
+    expect(page).to have_content 'Your question successfully created.'
+    expect(page).to have_content 'Test question'
+    expect(page).to have_content 'Test test test'
+  end
+
   scenario 'Authenticated user asks a question with errors'
   scenario 'Unauthenticated user tries to ask a question'
 end
