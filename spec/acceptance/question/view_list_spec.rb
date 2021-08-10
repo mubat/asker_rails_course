b944da75b517412b24be0138cf0b14d329259ab7
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-10 15:34:09 +0300

    add test to check: user can view questions list

---
+++ spec/acceptance/question/view_list_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'User can view list of questions', '
+  In order to see previously asked questions
+  As an authenticated user
+  I\'d like to be able to view list of questions
+' do
+
+  scenario 'View list of questions'
+end
