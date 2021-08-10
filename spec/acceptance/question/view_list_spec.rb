ad1a4e506b6174e645b673264abafdfc1c7cf657
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-10 15:37:49 +0300

    describe test to check that user can see asked questions

--- spec/acceptance/question/view_list_spec.rb
+++ spec/acceptance/question/view_list_spec.rb
@@ -6,5 +6,13 @@ feature 'User can view list of questions', '
   I\'d like to be able to view list of questions
 ' do
 
-  scenario 'View list of questions'
+  given!(:questions) { create_list(:question, 10) }
+
+  scenario 'View list of questions' do
+    visit questions_path
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
+  end
 end
