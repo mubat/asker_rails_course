0963260ba1537245f96f47d45d9b69a992fec841
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-14 15:02:25 +0300

    describe test that authorized user can not login with invalid password

--- spec/acceptance/authentication/login_spec.rb
+++ spec/acceptance/authentication/login_spec.rb
@@ -15,7 +15,14 @@ feature 'User can login', "
       expect(page).to have_content 'Signed in successfully.'
     end
 
-    scenario 'can not login with invalid password'
+    scenario 'can not login with invalid password' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: Faker::Internet.password(min_length: 8)
+      click_on 'Log in'
+
+      expect(page).to have_content "Invalid Email or password."
+      expect(current_path).to eq new_user_session_path
+    end
 
   end
 
