9788c7a3cc3695b8235d24da3314866c6df7d415
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-14 16:27:25 +0300

    link current_user to created Question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,9 +9,10 @@ class QuestionsController < ApplicationController
   def new; end
 
   def create
-    @question = Question.new(question_params)
-    if @question.save
-      redirect_to @question, notice: 'Your question successfully created.'
+    question = Question.new(question_params)
+    question.user = current_user
+    if question.save
+      redirect_to question, notice: 'Your question successfully created.'
     else
       render :new
     end
