1cde202c0b61e9ded91ce4a79c55b7ed41bdbf13
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-14 16:34:09 +0300

    create new questions from current_user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,8 +9,7 @@ class QuestionsController < ApplicationController
   def new; end
 
   def create
-    question = Question.new(question_params)
-    question.user = current_user
+    question = current_user.questions.new(question_params)
     if question.save
       redirect_to question, notice: 'Your question successfully created.'
     else
