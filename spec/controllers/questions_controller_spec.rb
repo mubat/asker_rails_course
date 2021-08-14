0865c413e1a89340185da476ed234edb1c52abb5
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-14 16:13:50 +0300

    add test to check that created user set as owner to created Question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -47,14 +47,21 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'POST #create' do
     before { login(user) }
+    let(:last_question) { Question.order(:created_at).last }
 
     context 'with valid attributes' do
+
       it 'saves a new question in the database' do
         expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
       end
       it 'redirects to show view' do
         post :create, params: { question: attributes_for(:question) }
         expect(response).to redirect_to assigns(:question)
+      end
+
+      it "should set current user question's owner" do
+        post :create, params: { question: attributes_for(:question) }
+        expect(question.user).to eq user
       end
     end
 
