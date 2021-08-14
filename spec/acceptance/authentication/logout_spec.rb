5e54bd420ea59f072ba9e72601eda0066d7f3de5
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-14 15:06:15 +0300

    add test to validate that user can logout

---
+++ spec/acceptance/authentication/logout_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User can logout', "
+  In order to allow to end user session
+  As an unauthenticated user
+  I'd like to be able to logout
+" do
+
+  scenario 'Authenticated user can logout' do
+  end
+
+  scenario 'Unauthenticated user can not logout' do
+  end
+
+end
+
