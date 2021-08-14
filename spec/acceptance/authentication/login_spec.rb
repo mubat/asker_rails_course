08b51382061775861d7b731559b2a31ef80d0c4f
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-14 15:03:25 +0300

    describe test that unauthorized user can not login

--- spec/acceptance/authentication/login_spec.rb
+++ spec/acceptance/authentication/login_spec.rb
@@ -26,6 +26,13 @@ feature 'User can login', "
 
   end
 
-  scenario 'Unregistered user can not login'
+  scenario 'Unregistered user can not login' do
+    fill_in 'Email', with: Faker::Internet.email
+    fill_in 'Password', with: Faker::Internet.password(min_length: 8)
+    click_on 'Log in'
+
+    expect(page).to have_content "Invalid Email or password."
+    expect(current_path).to eq new_user_session_path
+  end
 
 end
