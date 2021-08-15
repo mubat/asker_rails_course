a48e9a0089074f381321b37115c2b5f13be8f4c1
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-08-16 00:53:01 +0300

    add test to validate `author_of?` method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'check #author_of?' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:other_question) { create(:question) }
+
+    it 'passed if user is author of the question' do
+      expect(user).to be_author_of(question)
+    end
+
+    it 'failed if user is author of the other question' do
+      expect(user).to_not be_author_of(other_question)
+    end
+
+  end
 end
