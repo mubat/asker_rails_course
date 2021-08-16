aee215e2cad85583e04bcf46bef2e09064dc9fa4
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-08-16 21:07:48 +0300

    create action Answer#destroy

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,6 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_question, only: %i[create]
+  before_action :find_question, only: %i[create, destroy]
 
   def create
     @answer = @question.answers.new(answer_params)
@@ -8,6 +8,11 @@ class AnswersController < ApplicationController
     return render 'questions/show', locals: {question: @answer.question} unless @answer.save
 
     redirect_to question_path(@question)
+  end
+
+  def destroy
+    answer.destroy
+    redirect_to answer.question
   end
 
   private
@@ -18,5 +23,9 @@ class AnswersController < ApplicationController
 
   def find_question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer
+    @answer ||= params[:id] ? Answer.find(params[:id]) : Answer.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'questions#index'
 
   resources :questions do
-    resources :answers, only: %i[new create]
+    resources :answers, only: %i[new create destroy]
   end
 end
