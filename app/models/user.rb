7edf0d5cb731564accdd669a59d8e6bdc744c5cf
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-08-16 21:26:00 +0300

    link Answer with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   def author_of?(question)
     question.user_id == id
