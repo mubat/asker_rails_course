8e88d7daa62b3be9e058c08e75ac9a8662c0854a
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-26 08:53:55 +0300

    implement action Answers#update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'questions#index'
 
   resources :questions do
-    resources :answers, shallow: true, only: %i[new create edit destroy]
+    resources :answers, shallow: true, only: %i[new create update destroy]
   end
 end
