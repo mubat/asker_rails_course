2b34c7d038bb8cf360fe25bb0726ddbb146a809c
Oleg S <sergienko.oleg.92@gmail.com>
2021-08-26 08:54:26 +0300

    update controller test Answers#update

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -55,21 +55,34 @@ RSpec.describe AnswersController, type: :controller do
   describe 'PATCH #update' do
     let!(:answer) { create(:answer, question: question) }
 
-    context 'with valid attributes' do
-      it 'changes answer attributes' do
-        patch :update, params: { id: answer, answer: { body: 'new body' } }, format: :js
-        answer.reload
-        expect(answer.body).to eq 'new body'
+    describe 'by authenticated user' do
+      before { login(user) }
+
+      context 'with valid attributes' do
+        it 'changes answer attributes' do
+          patch :update, params: { id: answer, answer: { body: 'new body' } }, format: :js
+          answer.reload
+          expect(answer.body).to eq 'new body'
+        end
+
+        it 'renders update view' do
+          patch :update, params: { id: answer, answer: { body: 'new body' } }, format: :js
+          expect(response).to render_template :update
+        end
       end
 
-      it 'renders update view' do
-        patch :update, params: { id: answer, answer: { body: 'new body' } }, format: :js
-        expect(response).to render_template :update
+      context 'with valid attributes' do
+        it 'does not change answer attributes' do
+          expect do
+            patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) }, format: :js
+          end.to_not change(answer, :body)
+        end
+
+        it 'renders update view' do
+          patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) }, format: :js
+          expect(response).to render_template :update
+        end
       end
-    end
-
-    context 'with valid attributes' do
-
     end
   end
 
@@ -95,7 +108,7 @@ RSpec.describe AnswersController, type: :controller do
       before { login(another_user) }
 
       it "can't to destroy the answer" do
-        expect { delete :destroy, params: { id: answer, question_id: question }}.not_to change(Answer, :count)
+        expect { delete :destroy, params: { id: answer, question_id: question } }.not_to change(Answer, :count)
       end
     end
 
