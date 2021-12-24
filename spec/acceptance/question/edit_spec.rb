415c426788a382a336ab8f2423332be94c38c660
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-12-24 11:16:42 +0200

    7.2.1 describe test that User can update his question

--- spec/acceptance/question/edit_spec.rb
+++ spec/acceptance/question/edit_spec.rb
@@ -7,9 +7,33 @@ feature 'User can edit his own question', "
 " do
 
   given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
 
   describe 'Question creator' do
-    scenario "Can edit his question"
+    scenario "Can edit his question", js:true do 
+      login(user)
+      visit question_path(:question)
+
+      within '.question-data' do
+        expect(page).to have_link 'Edit'
+
+        click_on 'Edit'
+        
+        expect(page).to_not have_link 'Edit'
+        
+        fill_in 'Title', with: 'new title'
+        fill_in 'Body', with: 'new body'
+        click_on 'Save'
+        
+        expect(page).to_not have_field 'Title'
+        expect(page).to_not have_contect question.title
+        expect(page).to_not have_contect question.body
+        expect(page).to have_contect 'new title'
+        expect(page).to have_contect 'new body'
+        expect(page).to have_link 'Edit'
+      end
+
+    end
   end
 
   describe 'Other user' do
