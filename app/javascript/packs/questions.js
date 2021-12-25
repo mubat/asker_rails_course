910a774d7d10545d056913a6b044a0f221b167fe
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-12-25 11:28:17 +0200

    7.2.1 add link to edit Question

---
+++ app/javascript/packs/questions.js
@@ -0,0 +1,7 @@
+$(document).on('turbolinks:load', function () {
+    $('.question-data').on('click', '.edit-question', function (event) {
+        event.preventDefault();
+        let _this = $(this);
+        _this.hide();        
+    });
+});
