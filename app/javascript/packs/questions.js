46f3bd93babaaa452dee4d3f2a0dbfcc3975236a
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-12-25 15:10:08 +0200

    7.2.1 enable `update` of the Question via JS

--- app/javascript/packs/questions.js
+++ app/javascript/packs/questions.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function () {
     $('.question-data').on('click', '.edit-question', function (event) {
         event.preventDefault();
         let _this = $(this);
-        _this.hide();        
+        _this.addClass('hidden');        
         $(`form#form-edit-question`).removeClass('hidden');
  });
 });
