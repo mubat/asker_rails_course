13fbaab74dadaa5e0beed32b79aa9c8757de8096
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-12-26 23:47:56 +0200

    7.2.2 change to update data only of the updated Answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,7 +9,7 @@ class AnswersController < ApplicationController
   end
 
   def update
-    answer.update(answer_params)
+    answer.update(answer_params) if current_user.author_of?(answer)
     @question = answer.question
   end
 
