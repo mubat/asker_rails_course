15dfb140fa8cc56d41fcd0bcf1f3fe09ff1284e9
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-12-28 00:37:49 +0200

    7.2.6 put best Answer to the top of the list for Question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,24 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
 
+  before_validation :set_nil_for_false # nil and FALSE are considered the same
+
   validates :body, presence: true
-  validates_inclusion_of :is_best, in: [true, false, nil]
+  validates_inclusion_of :is_best, in: [true, false, nil] 
+
+  # use custom sorting to sort answers wit FALSE or nil after TRUE `is_best` value
+  scope :sort_by_best, -> { order(Arel.sql('case when is_best then 1 when is_best is null then 2 else 3 end')) }
 
   def make_best
     transaction do
-      question.best_answer.update(is_best: false) if question.best_answer
+      question.answers.where(is_best: true).update_all(is_best: false)
       update(is_best: true) 
     end
+  end
+
+  private 
+
+  def set_nil_for_false
+    @is_best = nil unless @is_best
   end
 end
