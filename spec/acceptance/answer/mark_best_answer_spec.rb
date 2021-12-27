e2198ae3b0da678fd5b13501028b47450185f0b2
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-12-27 14:43:01 +0200

    7.2.4 add acceptance spec: User can mark best Answer

--- spec/acceptance/answer/mark_best_answer_spec.rb
+++ spec/acceptance/answer/mark_best_answer_spec.rb
@@ -6,10 +6,25 @@ feature 'Author of the Question can mark best Answer', "
   I'd like to be able to mark best Answer
 " do
 
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, question: question, user: user, is_best: false) }
+
   scenario "Unauthenticated User can't choose best Answer"
 
   describe 'Authenticated user' do
-    scenario 'can mark best Answer'
+    scenario 'can mark best Answer' do
+      login(user)
+      visit question_path(question)
+
+      within "#answer-#{answer.id}" do
+        click 'Best answer'
+        answer.reload
+
+        expect(page).to have_content 'Best answer'
+        expect(answer.is_best).to be true
+      end
+    end
     scenario 'can mark only one best Answer'
     scenario "can't choose best Answer if not an author"
   end
