406a0c1f63cec6ab8d6257930098e6b4f0169211
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-12-27 15:22:32 +0200

    7.2.4 add option `is_best` at Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,5 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+  validates_inclusion_of :is_best, in: [true, false, nil]
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:user) }
 
   it { should validate_presence_of :body }
+ 
+  it { should allow_value(nil).for(:is_best) } # Rails validator `validate_inclusion_of` can't check nil. Watch validator description
+  it { should validate_inclusion_of(:is_best).in_array([true, false]) }
 end
