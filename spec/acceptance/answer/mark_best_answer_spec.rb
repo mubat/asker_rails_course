8639c532bd11088f91fc12fe6f3fb065cddf4064
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-12-27 19:11:52 +0200

    7.2.5 describe acceptance spec: Question's author can change best Answer

--- spec/acceptance/answer/mark_best_answer_spec.rb
+++ spec/acceptance/answer/mark_best_answer_spec.rb
@@ -30,7 +30,25 @@ feature 'Author of the Question can mark best Answer', "
         expect(answer.is_best).to be_truthy
       end
     end
-    scenario 'can mark only one best Answer'
+    
+    scenario 'can mark other Answer as best (old best Answer stays regular)', js: true do
+      old_best_answer = create(:answer, question: question, user: user, is_best: true)
+      visit question_path(question)
+
+      expect(find("#answer-#{old_best_answer.id}")).to have_content 'Best answer' 
+      
+      within "#answer-#{answer.id}" do
+        expect(page).to_not have_text 'Best answer'
+
+        click_on 'Make answer best'
+        
+        expect(page).to have_content 'Best answer'        
+        expect(page).to_not have_link 'Make answer best'  
+      end
+      
+      expect(find("#answer-#{old_best_answer.id}")).to_not have_content 'Best answer' 
+    end
+
     scenario "can't choose best Answer if not an author" do
       other_user = create(:user)
       other_question = create(:question, user: other_user)
