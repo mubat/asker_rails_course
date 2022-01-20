82b279620f8eaebbff22e4353eee5fbd7f692d72
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-01-21 00:29:39 +0200

    8.2. describe acceptance spec: can add an answer with attached file

--- spec/acceptance/answer/add_answer_spec.rb
+++ spec/acceptance/answer/add_answer_spec.rb
@@ -30,6 +30,20 @@ feature "Authorized user can add answer on the question's view", "
       expect(current_path).to eq question_path(question)
       expect(page).to have_content "Answer can't be blank"
     end
+
+    scenario 'Can add a new answer with attached files', js: true do
+      fill_in 'Answer', with: 'Test answer'
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+
+      click_on 'Answer it'
+
+      expect(current_path).to eq question_path(question)
+      within '.answers' do
+        expect(page).to have_content 'Test answer'
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
   scenario "Can't add a new answer" do
