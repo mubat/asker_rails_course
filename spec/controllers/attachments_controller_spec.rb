e96f95aae8c24e363b89224ec93a5e2523394dfe
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-01-30 15:01:47 +0200

    8.5. describe controller spec: not an author can't delete Question's attachment

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -16,7 +16,15 @@ RSpec.describe AttachmentsController, type: :controller do
           end.to change(ActiveStorage::Attachment, :count).by(-1)
         end
 
-        it 'should not remove attached file from not own question'
+        it 'should not remove attached file from not own question' do
+          other_user = create(:user)
+          login(other_user)
+
+          expect do
+            delete :destroy, params: { id: question.files.first }
+            question.reload
+          end.to_not change(ActiveStorage::Attachment, :count)
+        end
       end
     end
 
