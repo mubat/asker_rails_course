eaea2ce9da32a5d59f9a010984216029eecf9cc7
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-01-30 14:49:19 +0200

    8.5. describe controller spec: user can delete Question's attachment

---
+++ spec/controllers/attachments_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe AttachmentsController, type: :controller do
+  describe 'DELETE #destory' do
+    describe 'Question attachment' do
+      let(:user) { create(:user) }
+      let!(:question) { create(:question, files: [fixture_file_upload("#{Rails.root}/spec/rails_helper.rb")]) }
+
+      describe 'by authenticated user' do
+        before { login(user) }
+
+        it 'remove attached file from own 0question' do
+          expect do
+            delete :destroy, params: { id: question.files.first }
+            question.reload
+          end.to change(ActiveStorage::Attachment, :count).by(-1)
+        end
+
+        it 'should not remove attached file from not own question'
+      end
+    end
+
+    describe 'by unauthenticated user' do
+      it 'should not remove attached file from question'
+    end
+  end
+end
