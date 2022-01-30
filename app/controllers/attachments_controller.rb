e54ac92106b91526d58d6295a31318f1bd2bc0d5
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-01-30 14:53:32 +0200

    8.5. implement attachment deletion action for Question

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,5 +1,11 @@
 class AttachmentsController < ApplicationController
   def destroy
+    attachment.purge
+  end
 
+  private
+
+  def attachment
+    @attachment ||= ActiveStorage::Attachment.find(params[:id])
   end
 end
