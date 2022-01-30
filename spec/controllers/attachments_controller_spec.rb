6578b6270cef83b85bf6b3118970878ee564d501
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-01-30 16:30:18 +0200

    8.5. fix 'long line' warning

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe AttachmentsController, type: :controller do
   describe 'DELETE #destory' do
     let(:user) { create(:user) }
-    let!(:question) { create(:question, user: user, files: [fixture_file_upload("#{Rails.root}/spec/rails_helper.rb")]) }
+    let!(:question) do
+      create(:question, user: user, files: [fixture_file_upload("#{Rails.root}/spec/rails_helper.rb")])
+    end
 
     describe 'Question attachment' do
       describe 'by authenticated user' do
