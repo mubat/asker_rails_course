02d5f919c16b2e6f54c96436c4051d24814fcd13
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-04 00:07:49 +0200

    9.0.1. describe acceptance test: User can add link when asks question

---
+++ spec/acceptance/question/add_links_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'User can add links to question', "
+  In order to provide additional info to my question
+  As an question's author
+  I'd like to be able to add links
+" do
+  given(:user) { create(:user) }
+  given(:testing_url) { 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' }
+
+  scenario 'User adds link when asks question' do
+    login(user)
+    visit new_question_path
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Test test test'
+
+    fill_in 'Link name ', with: 'Very important link'
+    fill_in 'Url', with: testing_url
+
+    click_on 'Ask'
+
+    expect(page).to have_link 'My gist', href: testing_url
+  end
+end
