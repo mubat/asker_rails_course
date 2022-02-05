8152cfa16f4f72837b9892cd6a486deb8030a1f9
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-06 00:42:42 +0200

    9.1.1 rewrite test to check that User can add several links on Question's creation

--- spec/acceptance/question/add_links_spec.rb
+++ spec/acceptance/question/add_links_spec.rb
@@ -8,18 +8,27 @@ feature 'User can add links to question', "
   given(:user) { create(:user) }
   given(:testing_url) { 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' }
 
-  scenario 'User adds link when asks question' do
+  scenario 'User adds several links when asks question', js: true do
     login(user)
     visit new_question_path
 
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'Test test test'
 
+    click_on 'Add link'
+
     fill_in 'Link name', with: 'Very important link'
     fill_in 'Url', with: testing_url
 
+    click_on 'Add link'
+
+    within '.nested-fields:nth-of-type(2)' do
+      fill_in 'Link name', with: 'Very important link2'
+      fill_in 'Url', with: testing_url
+    end
     click_on 'Ask'
 
     expect(page).to have_link 'Very important link', href: testing_url
+    expect(page).to have_link 'Very important link2', href: testing_url
   end
 end
