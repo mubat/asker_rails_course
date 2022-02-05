33843645921d22ecb22a09fb23fe29e0ecd4545c
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-05 19:51:46 +0200

    9.0.2 describe acceptance test: User can add Links on Answer creation

---
+++ spec/acceptance/answer/add_links_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'User can add links to Answer', "
+  In order to provide additional info to my Answer
+  As an Answer's author
+  I'd like to be able to add links
+" do
+  given(:user) { create(:user) }
+  given!(:question) { create(:question) }
+  given(:testing_url) { 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' }
+
+  scenario 'User adds link when asks Answer' do
+    login(user)
+    visit question_path(question)
+
+    fill_in 'Answer', with: 'Test answer'
+
+    fill_in 'Link name', with: 'Very important link'
+    fill_in 'Url', with: testing_url
+
+    click_on 'Answer it'
+    within '.answers' do
+      expect(page).to have_link 'Very important link', href: testing_url
+    end
+  end
+end
