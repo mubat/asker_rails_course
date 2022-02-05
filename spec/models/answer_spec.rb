a7316b01b54664de859ddebc8cd1750b7e16fdc2
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-05 19:52:22 +0200

    9.0.2 add links relationship validation to Answer's model test

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to(:question).required }
+  it { should have_many(:links).dependent(:destroy) }
   it { should belong_to(:user) }
 
   it { should validate_presence_of :body }
- 
- # Rails validator `validate_inclusion_of` can't check nil. Watch validator description
+
+  it { should accept_nested_attributes_for :links }
+
+  # Rails validator `validate_inclusion_of` can't check nil. Watch validator description
   it { should allow_value(nil).for(:is_best) } 
   it { should allow_value(true).for(:is_best) }
   it { should allow_value(false).for(:is_best) }
