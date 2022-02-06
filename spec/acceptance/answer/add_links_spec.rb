810d11437e396e4202bb8a8669dd1dd7012cbe19
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-06 18:12:37 +0200

    9.1.2 refactor tests at `User can add links to answer`

--- spec/acceptance/answer/add_links_spec.rb
+++ spec/acceptance/answer/add_links_spec.rb
@@ -7,46 +7,46 @@ feature 'User can add links to Answer', "
 " do
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
+  given(:testing_url_name) { "Answer's link" }
   given(:testing_url) { 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' }
+  given(:wrong_url) { 'http:/wrongUrl' }
 
-  scenario 'User adds link when asks Answer', js: true do
-    login(user)
-    visit question_path(question)
+  describe 'Authenticated user' do
+    background do
+      login(user)
+      visit question_path(question)
+      fill_in 'Answer', with: 'Test answer'
+    end
 
-    fill_in 'Answer', with: 'Test answer'
+    scenario 'adds link when asks Answer', js: true do
+      click_on 'Add link'
 
-    click_on 'Add link'
-
-    fill_in 'Link name', with: 'Very important link'
-    fill_in 'Url', with: testing_url
-
-    click_on 'Add link'
-
-    within '.nested-fields:nth-of-type(2)' do
-      fill_in 'Link name', with: 'Very important link2'
+      fill_in 'Link name', with: 'Very important link'
       fill_in 'Url', with: testing_url
+
+      click_on 'Add link'
+
+      within '.nested-fields:nth-of-type(2)' do
+        fill_in 'Link name', with: 'Very important link2'
+        fill_in 'Url', with: testing_url
+      end
+
+      click_on 'Answer it'
+      within '.answers' do
+        expect(page).to have_link 'Very important link', href: testing_url
+      end
     end
 
-    click_on 'Answer it'
-    within '.answers' do
-      expect(page).to have_link 'Very important link', href: testing_url
+    scenario "can't adds link with invalid URL when asks question", js: true do
+      click_on 'Add link'
+
+      fill_in 'Link name', with: 'Very important link'
+      fill_in 'Url', with: 'http:/wrongUrl'
+
+      click_on 'Answer it'
+
+      expect(page).to_not have_link 'Very important link'
+      expect(page).to have_content 'url is not a valid URL'
     end
   end
-
-  scenario "User can't adds link with invalid URL when asks question", js: true do
-    login(user)
-    visit question_path(question)
-    fill_in 'Answer', with: 'Test answer'
-
-    click_on 'Add link'
-
-    fill_in 'Link name', with: 'Very important link'
-    fill_in 'Url', with: 'http:/wrongUrl'
-
-    click_on 'Answer it'
-
-    expect(page).to_not have_link 'Very important link'
-    expect(page).to have_content 'url is not a valid URL'
-  end
-
 end
