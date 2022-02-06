49fc158e2b10c467a5cfe888efd70b3e0fe51cf8
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-07 01:11:37 +0200

    9.1.4 describe acceptance test: User can remove Links from Question

---
+++ spec/acceptance/question/delete_links_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature 'User can delete links from Question', "
+  In order to allow to remove links my Question
+  As an Question's author
+  I'd like to be able to delete links
+" do
+
+  describe 'Authenticated user' do
+    given(:user) { create :user }
+    given(:question) { create :question, user: user }
+    given!(:links) { create_list :link, 2, linkable: question }
+
+    scenario 'can remove links from question', js: true do
+      login(user)
+      visit question_path(question)
+
+      within '.question-data' do
+        expect(page).to have_link links.first.name
+        expect(page).to have_link links.second.name
+
+        within "#link-#{links.second.id}" do
+          click_on 'Delete link'
+        end
+
+        expect(page).to have_link links.first.name
+        expect(page).to_not have_link links.second.name
+      end
+    end
+
+    scenario "can't remove links of another author's answer"
+  end
+
+  describe "Unauthorized User can't delete links from Question"
+end
