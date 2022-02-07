55deca9bd136695b7704c756a9f11764d1549420
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-07 20:55:30 +0200

    9.1.4 describe acceptance test: User can't delete link from other author's Answer

--- spec/acceptance/answer/delete_links_spec.rb
+++ spec/acceptance/answer/delete_links_spec.rb
@@ -30,7 +30,16 @@ feature 'User can delete links from Answer', "
       end
     end
 
-    scenario "can't remove links of another author's answer"
+    scenario "can't remove links of another author's answer" do
+      other_user = create(:user)
+      login(other_user)
+      visit question_path(answer.question)
+
+      within "#answer-#{answer.id}" do
+        expect(page).to have_link links.first.name
+        expect(page).to_not have_link 'Delete link'
+      end
+    end
   end
 
   describe "Unauthorized User can't delete links from Answer"
