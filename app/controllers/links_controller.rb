76c893da63477807e8f0dc604678ae6b933bfc6d
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-07 21:18:38 +0200

    9.1.4 allow to delete Link only for author

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,8 @@
 class LinksController < ApplicationController
+  before_action :authenticate_user!
+  
   def destroy
-    link.destroy
+    link.destroy if current_user.author_of?(link.linkable)
   end
 
   private
