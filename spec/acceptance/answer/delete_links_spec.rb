1007f3420966d9e74c593efcc03600c593b6592e
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-07 22:02:32 +0200

    9.1.4 describe acceptance test: Guest can't delete link from Answer

--- spec/acceptance/answer/delete_links_spec.rb
+++ spec/acceptance/answer/delete_links_spec.rb
@@ -6,10 +6,11 @@ feature 'User can delete links from Answer', "
   I'd like to be able to delete links
 " do
 
+  given(:user) { create :user }
+  given(:answer) { create :answer, question: create(:question), user: user }
+  given!(:links) { create_list :link, 2, linkable: answer }
+
   describe 'Authenticated user' do
-    given(:user) { create :user }
-    given(:answer) { create :answer, question: create(:question), user: user }
-    given!(:links) { create_list :link, 2, linkable: answer }
 
     scenario 'can remove links from answer', js: true do
       login(user)
@@ -42,5 +43,12 @@ feature 'User can delete links from Answer', "
     end
   end
 
-  describe "Unauthorized User can't delete links from Answer"
+  scenario "Unauthorized User can't delete links from Answer" do
+      visit question_path(answer.question)
+      
+      within "#answer-#{answer.id}" do
+        expect(page).to have_link links.first.name
+        expect(page).to_not have_link 'Delete link'
+      end
+  end
 end
