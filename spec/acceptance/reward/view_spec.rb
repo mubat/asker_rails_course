5be920b81d8bf1357095971b3f93541a5e8dd759
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-10 00:39:41 +0200

    9.2.4 Describe acceptance test: User can see his Rewards

--- spec/acceptance/reward/view_spec.rb
+++ spec/acceptance/reward/view_spec.rb
@@ -6,7 +6,21 @@ feature "User can view his Rewards", "
   I'd like to see all my Rewards with Questions
 " do
   describe 'Authenticated user' do
-    describe 'see his Rewards'
+    given(:user) { create :user }
+    given(:question) { create :question, user: create(:user) }
+    given!(:rewards) { create_list :reward, 2, user: user }
+
+    scenario 'see his Rewards' do
+      login(user)
+      visit questions_path
+      click_on 'My rewards'
+
+      rewards.each do |reward|
+        expect(page).to have_content reward.question.title
+        expect(page).to have_content reward.name
+        expect(page).to have_css("img[src*='#{reward.image.filename}']")
+      end
+    end
 
     describe "see 'No rewards' message when has no one Reward"
   end
--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :reward do
-    name { "MyString" }
+    name { Faker::Fantasy::Tolkien.character }
+    question
     image { Rack::Test::UploadedFile.new('spec/fixtures/test_img.jpg', 'image/jpg') }
   end
 end
