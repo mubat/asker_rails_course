a51857de685a410f8a7a6ed6dd307cf7d95636ae
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-09 21:07:20 +0200

    9.2.1 update Reward's model test: should belongs to Question and User separately

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Reward, type: :model do
-  it { should belong_to :rewardable }
+  it { should belong_to :question }
+  it { should belong_to(:user).optional(true) }
 
   it { should validate_presence_of :name }
 
