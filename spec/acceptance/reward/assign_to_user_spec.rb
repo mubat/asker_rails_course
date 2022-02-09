ed61027215f7bedfd256a8c6a3a4fbc9c38c1982
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-09 23:07:35 +0200

    9.2.3 describe acceptance test: Answer's author take a Reward when an Answer mark as best

--- spec/acceptance/reward/assign_to_user_spec.rb
+++ spec/acceptance/reward/assign_to_user_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 
 feature "User takes Reward when Question's author choose best Answer", "
   In order to reward User with best Answer
-  As and Question's author
+  As a Question's author
   I'd like to be able to give Reward to Answer's author when mark best answer
 " do
 
   given(:question_author) { create(:user) }
-  given(:question) { create(:question, user: question_author) }
+  given!(:question) { create(:question, user: question_author) }
+  given!(:reward) { create(:reward, question: question) }
   given(:answer_author) { create(:user) }
-  given(:answer) { create(:answer, user: answer_author) }
+  given!(:answer) { create(:answer, user: answer_author, question: question) }
 
-  scenario 'User takes Reward when his Answer marked as best' do
+  scenario 'User takes Reward when his Answer marked as best', js: true do
+    login(question_author)
+    visit question_path(question)
 
+    within "#answer-#{answer.id}" do
+      click_on 'Make answer best'
+    end
+
+    expect(page).to have_content "User #{answer_author.email} received a #{reward.name} reward"
+    expect(answer_author.awards).to eq [award]
   end
 
   scenario "Question's author doesn't take a reward when mark his answer as best"
--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :reward do
     name { "MyString" }
+    image { Rack::Test::UploadedFile.new('spec/fixtures/test_img.jpg', 'image/jpg') }
   end
 end
