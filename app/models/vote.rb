0577a2181629920da6dcbaafb892f7e58e2036f1
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-14 22:51:18 +0200

    10.1 update Votes model and his test

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,10 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
+  enum degree: { like: 1, dislike: -1 }
+
+  validates :degree, presence: true
+
   def like
     self.degree = 1
   end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  it { should belong_to(:user) }
-  it { should belong_to(:votable) }
+  it { should belong_to :user }
+  it { should belong_to :votable }
 
   it { should validate_presence_of :degree }
-  it { should validate_presence_of :user }
-  it { should validate_presence_of :votable }
+
+  it { should define_enum_for(:degree).with_values({ like: 1, dislike: -1 }) }
 end
