b35c032a9b2b56b588197d3cc6d954c4c0878c35
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-17 23:57:40 +0200

    10.1 describe controller test: Response after deleting Vote has an actual Votable rating

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe VotesController, type: :controller do
 
   describe 'DELETE #destroy' do
     let(:user) { create(:user) }
-    let!(:vote) { create(:vote, user: user) }
+    let!(:answer) { create(:answer) }
+    let!(:vote) { create(:vote, user: user, votable: answer) }
 
     context 'by authenticated user' do
       before { login(user) }
@@ -12,6 +13,12 @@ RSpec.describe VotesController, type: :controller do
       context "by Vote's owner" do
         it 'can delete' do
           expect { delete :destroy, params: { id: vote }, format: :json }.to change(Vote, :count).by(-1)
+        end
+
+        it 'response with actual Votable rating' do
+          delete :destroy, params: { id: vote }, format: :json
+          parsed_body = JSON.parse(response.body)
+          expect(parsed_body['rating']).to eq 0
         end
 
         it "returns status 'OK'" do
