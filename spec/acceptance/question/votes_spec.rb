53d25378af9a1658ce8bfb8ffe2bb1a894851992
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-19 21:38:53 +0200

    10 describe rest of acceptance tests: User can vote on a Question

--- spec/acceptance/question/votes_spec.rb
+++ spec/acceptance/question/votes_spec.rb
@@ -33,14 +33,53 @@ feature 'User can set his vote on a Question', "
       end
     end
 
-    scenario "can't set a vote to his Question"
+    scenario "can't set a vote to his Question" do
+      visit question_path(create(:question, user: user))
 
-    scenario 'can remove his previously set vote'
+      within '.question-data' do
+        expect(page).to have_no_link 'Like'
+        expect(page).to have_no_link 'Dislike'
+      end
+    end
+    scenario 'can remove his previously set vote', js: true do
+      create(:vote, votable: question, user: user)
+      visit question_path(question)
+
+      within '.question-data' do
+
+        expect(page).to have_no_link 'Like'
+        expect(page).to have_no_link 'Dislike'
+
+        click_on 'Reset vote'
+
+        expect(page).to have_link 'Like'
+        expect(page).to have_link 'Dislike'
+        expect(page).to have_no_link 'Reset vote'
+      end
+    end
 
     describe 'changes a rating' do
-      scenario 'after set a vote'
+      scenario 'after set a vote', js: true do
+        visit question_path(question)
 
-      scenario 'after remove his vote'
+        within '.question-data' do
+          expect(page).to have_content 'Rating: 0'
+          click_on 'Dislike'
+          expect(page).to have_content 'Rating: -1'
+        end
+      end
+
+      scenario 'after remove his vote', js: true do
+        create(:vote, votable: question, user: user, degree: :like)
+        visit question_path(question)
+
+        within '.question-data' do
+
+          expect(page).to have_content 'Rating: 1'
+          click_on 'Reset vote'
+          expect(page).to have_content 'Rating: 0'
+        end
+      end
     end
   end
 
@@ -54,10 +93,24 @@ feature 'User can set his vote on a Question', "
     end
   end
 
-  describe 'User can view Question rating' do
-    scenario 'when several votes presents'
+  describe 'User can view Answer rating' do
+    scenario 'when several votes presents' do
+      create_list(:vote, 8, votable: question, degree: :like)
+      create_list(:vote, 5, votable: question, degree: :dislike)
+      visit question_path(question)
 
-    scenario 'when no one votes presents'
+      within '.question-data' do
+        expect(page).to have_content 'Rating: 3' # 8 likes - 5 dislikes
+      end
+    end
+
+    scenario 'when no one votes presents' do
+      visit question_path(question)
+
+      within '.question-data' do
+        expect(page).to have_content 'Rating: 0'
+      end
+    end
   end
 end
 
