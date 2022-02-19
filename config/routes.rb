6d322c932aaaf906cb7f98a965ec36beca037d9e
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-19 20:43:40 +0200

    10.1 create route concern 'votable' for Questions and Answers

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'questions#index'
 
-  resources :questions do
-    resources :answers, shallow: true, only: %i[new create update destroy] do
+  concern :votable do
+    member do
+      patch :like
+      patch :dislike
+    end
+  end
+
+  resources :questions, concerns: [:votable] do
+    resources :answers, shallow: true, only: %i[new create update destroy], concerns: [:votable] do
       member do
         patch :mark_best
-        patch :like
-        patch :dislike
       end
     end
   end
