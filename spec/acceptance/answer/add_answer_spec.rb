ad31d5298ecc72ca6406237049cd37bed9c94372
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2022-02-21 00:34:27 +0200

    11.1 describe acceptance tests: created Answer appears for all users with opened Question's page

--- spec/acceptance/answer/add_answer_spec.rb
+++ spec/acceptance/answer/add_answer_spec.rb
@@ -44,6 +44,141 @@ feature "Authorized user can add answer on the question's view", "
         expect(page).to have_link 'spec_helper.rb'
       end
     end
+
+    context 'multiple sessions', js: true do
+      describe 'Answer appears' do
+        scenario 'for authorized user' do
+          Capybara.using_session('creator') do
+            visit question_path(question)
+          end
+
+          Capybara.using_session('watcher') do
+            login(create(:user))
+            visit question_path(question)
+          end
+
+          Capybara.using_session('creator') do
+            within '.new_answer' do
+              fill_in 'Answer', with: 'Test answer'
+              attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+
+              click_on 'Answer it'
+
+              within '.answers' do
+                expect(page).to have_content 'Test answer'
+                expect(page).to have_link 'rails_helper.rb'
+                expect(page).to have_link 'spec_helper.rb'
+              end
+            end
+          end
+
+          Capybara.using_session('watcher') do
+            within '.answers' do
+              expect(page).to have_content 'Test answer'
+              expect(page).to have_link 'rails_helper.rb'
+              expect(page).to have_link 'spec_helper.rb'
+              expect(page).to_not have_link 'Edit'
+              expect(page).to_not have_link 'Delete'
+              expect(page).to_not have_link 'Make answer best'
+              #TODO: add Comments validation
+            end
+          end
+        end
+
+        scenario 'for unauthorized user' do
+          Capybara.using_session('creator') do
+            login(user)
+            visit question_path(question)
+          end
+
+          Capybara.using_session('watcher') do
+            visit question_path(question)
+          end
+
+          Capybara.using_session('creator') do
+            within '.new_answer' do
+              fill_in 'Answer', with: 'Test answer'
+              attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+
+              click_on 'Answer it'
+
+              within '.answers' do
+                expect(page).to have_content 'Test answer'
+                expect(page).to have_link 'rails_helper.rb'
+                expect(page).to have_link 'spec_helper.rb'
+              end
+            end
+          end
+
+          Capybara.using_session('watcher') do
+            within '.answers' do
+              expect(page).to have_content 'Test answer'
+              expect(page).to have_link 'rails_helper.rb'
+              expect(page).to have_link 'spec_helper.rb'
+              expect(page).to_not have_link 'Edit'
+              expect(page).to_not have_link 'Delete'
+              expect(page).to_not have_link 'Make answer best'
+              #TODO: add Comments validation
+            end
+          end
+        end
+
+        scenario "for Question's author with link 'Make best'" do
+          Capybara.using_session('creator') do
+            login(user)
+            visit question_path(question)
+          end
+
+          Capybara.using_session('question-owner') do
+            login(question.user)
+            visit question_path(question)
+          end
+
+          Capybara.using_session('creator') do
+            within '.new_answer' do
+              fill_in 'Answer', with: 'Test answer'
+
+              click_on 'Answer it'
+
+              within '.answers' do
+                expect(page).to have_content 'Test answer'
+              end
+            end
+          end
+
+          Capybara.using_session('question-owner') do
+            within '.answers' do
+              expect(page).to have_content 'Test answer'
+              expect(page).to have_link 'Make answer best'
+              expect(page).to_not have_link 'Edit'
+              expect(page).to_not have_link 'Delete'
+            end
+          end
+        end
+      end
+
+      scenario "Answer doesn't appears on other Questions" do
+        Capybara.using_session('creator') do
+          login(user)
+          visit question_path(question)
+        end
+
+        Capybara.using_session('watcher') do
+          visit question_path(create(:question))
+        end
+
+        Capybara.using_session('creator') do
+          within '.new_answer' do
+            fill_in 'Body', with: 'Test text123-./'
+            click_on 'Answer it'
+          end
+        end
+
+        Capybara.using_session('other_question') do
+          expect(page).not_to have_content 'Test text123-./'
+        end
+      end
+    end
   end
 
   scenario "Can't add a new answer" do
